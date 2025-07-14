f98b22e97517e491be188405d47fe21be7a91d18
Uttam Gupta <uttamgupta0908@gmail.com>
2025-07-14 22:41:16 +0530

    commit

--- App.tsx
+++ App.tsx
@@ -1,28 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import "./global.css"
+import { NavigationContainer } from '@react-navigation/native';
+import { View } from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
 
+export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+    <View className="flex-1">
+      {/* <TabNavigator /> */}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
