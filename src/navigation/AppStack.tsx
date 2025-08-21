95d04e9af2e04143f6e65c4db3fab674261fe8bc
Uttam Gupta <uttamgupta0908@gmail.com>
2025-08-21 23:41:24 +0530

    feat: Add AppStack navigation component with initial screen setup

---
+++ src/navigation/AppStack.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TabNavigator from './TabNavigator';
+import ExerciseDetail from '../components/ExerciseDetail';
+import WorkoutDetail from '../components/WorkoutDetail';
+import WorkoutRecord from '../components/WorkoutRecord';
+import ActiveWorkout from '../components/ActiveWorkout';
+import ExerciseSelectionModel from '../components/ExerciseSelectionModel';
+
+const Stack = createNativeStackNavigator();
+
+export default function AppStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="TABS" component={TabNavigator} />
+      <Stack.Screen
+        name="ExerciseDetail"
+        component={ExerciseDetail}
+        options={{
+          presentation: 'modal',
+          gestureEnabled: true,
+          animationTypeForReplace: 'push',
+        }}
+      />
+      <Stack.Screen name="WorkoutDetail" component={WorkoutDetail} />
+      <Stack.Screen
+        name="WorkoutRecord"
+        component={WorkoutRecord}
+        // options={{
+        //   headerShown: true,
+        //   headerTitle: 'Workout Record',
+        //   headerBackTitle: 'History',
+        // }}
+      />
+      <Stack.Screen
+        name="ActiveWorkout"
+        component={ActiveWorkout}
+        options={{
+          headerShown: false,
+          headerTitle: 'ActiveWorkout',
+          headerBackTitle: 'Create',
+        }}
+      />
+      {/* <Stack.Screen
+        name="ExerciseSelectionModel"
+        component={ExerciseSelectionModel}
+      /> */}
+    </Stack.Navigator>
+  );
+}
