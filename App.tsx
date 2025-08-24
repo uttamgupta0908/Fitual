bee167d01f3ea34a6037d7cac29d077aaccd2ffe
Uttam Gupta <uttamgupta0908@gmail.com>
2025-08-25 01:48:14 +0530

    refactor: Update API_URL in environment file and enhance navigation reference in App component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { createRef } from 'react';
+import {
+  NavigationContainer,
+  NavigationContainerRef,
+} from '@react-navigation/native';
 
 import './global.css';
 import RootNavigator from './src/navigation/RootNavigator';
 import { AuthProvider } from './src/context/AuthContext';
+
+export const navigationRef = createRef<NavigationContainerRef<any>>();
+
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
       <AuthProvider>
         <RootNavigator />
       </AuthProvider>
