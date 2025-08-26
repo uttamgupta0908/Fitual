068e64aefd2763201805049796892eea1ec1287e
Uttam Gupta <uttamgupta0908@gmail.com>
2025-08-26 23:52:06 +0530

    refactor: Remove SignIn screen from AppStack navigation

--- src/navigation/AppStack.tsx
+++ src/navigation/AppStack.tsx
@@ -47,7 +47,6 @@ export default function AppStack() {
         name="ExerciseSelectionModel"
         component={ExerciseSelectionModel}
       /> */}
-      <Stack.Screen name="SignIn" component={SignInScreen} />
     </Stack.Navigator>
   );
 }
