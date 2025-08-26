61b23c54ac871a2504e6fd195492899c9955e5dd
Uttam Gupta <uttamgupta0908@gmail.com>
2025-08-26 23:52:36 +0530

    refactor: Standardize screen names in AuthLayout component

--- src/screens/(auth)/index.tsx
+++ src/screens/(auth)/index.tsx
@@ -1,16 +1,13 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignInScreen from './SignInScreen';
 import SignUpScreen from './SignUpScreen';
-import ProfileScreen from './ProfileScreen';
 const Stack = createNativeStackNavigator();
 
 export default function AuthLayout() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="SignUp" component={SignUpScreen} />
-      <Stack.Screen name="SignIn" component={SignInScreen} />
-
-      {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+      <Stack.Screen name="SIGN_UP" component={SignUpScreen} />
+      <Stack.Screen name="SIGN_IN" component={SignInScreen} />
     </Stack.Navigator>
   );
 }
